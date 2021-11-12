9ec15efa961146387cfe1a5db2da172e048d13f7
KPMoon <mkpong8202@gmail.com>
2021-11-12 14:29:40 +0900

    UserDao JdbcTemplate + 익명함수로 변경

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -8,75 +8,38 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
-    //private ConnectionMaker connectionMaker;
-    private DataSource dataSource;
+    
     private JdbcTemplate jdbcTemplate;
-    // public void setConnectionMaker(ConnectionMaker connectionMaker) {
-    //     this.connectionMaker  = connectionMaker;
-    // }
+
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void add(User user) {
-        //Connection c = connectionMaker.makeConnection();
-        // Connection c = dataSource.getConnection();
-        // PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
-        
-        // ps.setString(1, user.getId());
-        // ps.setString(2, user.getName());
-        // ps.setString(3, user.getPassword());
-        
-        // ps.executeUpdate();
-
-        // ps.close();
-        // c.close();
         this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(String id) throws SQLException {
-        // Connection c = connectionMaker.makeConnection();
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-        
-        User user = null;
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-        }
-        
-        rs.close();
-        ps.close();
-        c.close();
-        
-        if (user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
+    public User get(String id)  {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id}, new RowMapper<User>() {
+            public User mapRow(ResultSet rSet, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rSet.getString("id"));
+                user.setName(rSet.getString("name"));
+                user.setPassword(rSet.getString("password"));
+                return user;
+            }
+        });
     }
 
     public void deleteAll() {
-        // Connection c = dataSource.getConnection();
-        // PreparedStatement ps = c.prepareStatement("delete from users");
-
-        // ps.executeUpdate();
-
-        // ps.close();
-        // c.close();
         this.jdbcTemplate.update(new PreparedStatementCreator() {
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException{
                 return connection.prepareStatement("delete from users");
@@ -85,17 +48,6 @@ public class UserDao {
     }
 
     public int getCount() {
-        // Connection c = dataSource.getConnection();
-        // PreparedStatement ps = c.prepareStatement("select count(id) as cnt from users");
-
-        // ResultSet rSet = ps.executeQuery();
-
-        // rSet.next();
-        // int userCount = rSet.getInt("cnt");
-        // rSet.close();
-        // ps.close();
-        // c.close();
-        // return userCount;
         return this.jdbcTemplate.query(new PreparedStatementCreator() {
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
                 return connection.prepareStatement("select count(id) as cnt from users");
