9c4f06f0fb4ef2ab3458f45265b0f84c9a11bf97
KPMoon <mkpong8202@gmail.com>
2021-12-13 14:47:12 +0900

    불필요 import 제거

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,10 +1,10 @@
-package springbook.user.dao;
+// package springbook.user.dao;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 // @Configuration
 // public class DaoFactory {
