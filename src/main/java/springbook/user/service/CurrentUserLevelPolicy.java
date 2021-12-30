e22c5c64b7cc2b0afa7be67d4d624ab8cb873a39
KPMoon <mkpong8202@gmail.com>
2021-12-30 09:22:16 +0900

    1. javax mail 사용한 메일전송 구현 2. 메일서버 없으므로 에러남

--- src/main/java/springbook/user/service/CurrentUserLevelPolicy.java
+++ src/main/java/springbook/user/service/CurrentUserLevelPolicy.java
@@ -3,9 +3,17 @@ package springbook.user.service;
 import static springbook.user.service.UserService.MIN_LOGIN_COUNT_FOR_SILVER;
 import static springbook.user.service.UserService.MIN_RECOMMAND_COUNT_FOR_GOLD;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import springbook.user.dao.UserDao;
-import springbook.user.domain.User;
 import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
 public class CurrentUserLevelPolicy implements UserLevelPolicy {
     
@@ -19,8 +27,26 @@ public class CurrentUserLevelPolicy implements UserLevelPolicy {
     public void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendEmail(user);
     }
     
+    public void sendEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session session = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress("userAdmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText(user.getName() + "님의 등급이" + user.getLevel() + "로 변경되었습니다.");
+            Transport.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public boolean canUpgradeLevel(User user) {
         Level currentLevel = user.getLevel();
