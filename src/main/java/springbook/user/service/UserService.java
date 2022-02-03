b78b9af55a4af2eaa598c0503ef4b1e7ceb770b8
KPMoon <mkpong8202@gmail.com>
2022-02-03 15:23:13 +0900

    1. 설정에 tx namespace 적용 2. 트렌젝션 경계설정 레벨 통일을 위해 service를 통해 dao 접근하도록 수정

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,11 +1,24 @@
 package springbook.user.service;
 
+import java.util.List;
+
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 public interface UserService {
     public void add(User user);
-    public void upgradeLevels() throws Exception;
+
+    public User get(String id);
+
+    public List<User> getAll();
+
+    public void deleteAll();
+
+    public void update(User user);
+
+    public void upgradeLevels();
+
     public void setUserLevelPolicy(UserLevelPolicy userLevelPolicy);
+
     public void setUserDao(UserDao userDao);
 }
